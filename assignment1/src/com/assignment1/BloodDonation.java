d95ec1de36193289125a522619182d8ac8910c71
HP <HP@DESKTOP-OVH9QBL>
2025-06-30 22:12:45 +0530

    Assignment 1 of java language

---
+++ assignment1/src/com/assignment1/BloodDonation.java
@@ -0,0 +1,17 @@
+package com.assignment1;
+
+public class BloodDonation {
+
+	public static void main(String[] args) {
+		int age = 20;     // You can change this value
+        int weight = 55;  // You can change this value
+
+        if (age >= 18 && weight >= 50) {
+            System.out.println("Person is eligible for blood donation.");
+        } else {
+            System.out.println("Person is not eligible for blood donation.");
+        }
+
+	}
+
+}
---
+++ assignment1/src/com/assignment1/Voting.java
@@ -0,0 +1,16 @@
+package com.assignment1;
+
+public class Voting {
+
+	public static void main(String[] args) {
+		int age = 20; // You can change this value to test with different ages
+
+        if (age >= 18) {
+            System.out.println("Person is eligible for voting.");
+        } else {
+            System.out.println("Person is not eligible for voting.");
+        }
+
+	}
+
+}
