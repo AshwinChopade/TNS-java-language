f1c835c59e44d0b10ea1ec65b662909a5f4f09e4
Ashwin Chopade <ashwinchopade100@gmail.com>
2025-07-28 18:00:26 +0530

    Conditional Statements and Looping Statements

---
+++ DayTwoConditional_and_LoopingStatement/src/com/daytwo/LoopingStatements/DoWhileLoopDemo.java
@@ -0,0 +1,14 @@
+package com.daytwo.LoopingStatements;
+
+public class DoWhileLoopDemo {
+	public static void main(String[] args) {
+
+		int i =10;
+		do {
+			System.out.println(i);
+			i++;
+		}
+		while(i<=1);
+	}
+
+}
