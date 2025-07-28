090b613a6ddd9535e188452852d730fa83487257
Ashwin Chopade <ashwinchopade100@gmail.com>
2025-07-28 18:07:37 +0530

    Access Modifier

---
+++ DayFourAccessModifiers/src/com/dayfour/accessmodifiers/ExecutorDemo.java
@@ -0,0 +1,14 @@
+package com.dayfour.accessmodifiers;
+
+public class ExecutorDemo {
+	public static void main(String[] args) {
+
+		Base b = new Base();
+		
+		b.methodDefault();
+		b.methodProtected();
+		b.methodPublic();
+				
+	}
+
+}
